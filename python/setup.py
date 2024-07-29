fcc139fc44ab52f220d636b9c6d89b12d2723dca
Alena Chaikouskaya <alec@equinor.com>
2024-07-29 20:58:33 +0200

    Remove test require
    Tests are failing just fine

--- python/setup.py
+++ python/setup.py
@@ -40,7 +40,6 @@ skbuild.setup(
                       'pybind11 >= 2.3',
                       'setuptools_scm',
     ],
-    tests_require = ['pytest'],
     # we're building with the pybind11 fetched from pip. Since we don't rely on
     # a cmake-installed pybind there's also no find_package(pybind11) -
     # instead, the get include dirs from the package and give directly from
